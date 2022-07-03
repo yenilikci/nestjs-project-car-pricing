75e73088fe2645c23dd4975fb6a7b6d2ac72d770
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-03 22:02:59 +0300

    add user dto, serialization in the interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,17 +8,21 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // Run somethin before a request is handled
     // by the request handler
-    console.log('Im running before the handler', context);
+    // console.log('Im running before the handler', context);
 
     return handler.handle().pipe(
       map((data: any) => {
         // run something before the response is sent out
-        console.log('Im running before response is sent out', data);
+        // console.log('Im running before response is sent out', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
