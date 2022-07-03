128fa1fd4c8f7d5f9f0ed064313448c76750f3d3
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-03 16:14:03 +0300

    add typeorm hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,4 +8,19 @@ export class User {
   email: string;
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id: ' + this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id: ' + this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id: ' + this.id);
+  }
 }
