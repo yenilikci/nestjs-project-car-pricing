0cb017f7d0ae1172adfd9927e1821544fc655cde
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-04 22:21:11 +0300

    creating a user

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -27,8 +27,10 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex');
 
     // Create a new user and save it
+    const user = await this.usersService.create(email, result);
 
     // return the user
+    return user;
   }
 
   signin() {}
