740c12ee842d23900f6943f27ec1e740f9064f50
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-11 10:49:41 +0300

    not super effective tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -49,4 +49,24 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async (done) => {
+    fakeUsersService.findOne = () => null;
+    try {
+      await controller.findUser('1');
+    } catch (err) {
+      done();
+    }
+  });
 });
