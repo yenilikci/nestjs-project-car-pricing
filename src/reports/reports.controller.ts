8340620cd4d0fb4cc17f882ef58456a7488d1573
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-11 23:09:16 +0300

    creating query builder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -25,7 +25,7 @@ export class ReportsController {
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    console.log(query);
+    return this.reportsService.createEstimate(query);
   }
 
   @Post()
