5352eaed30623e9cb3ca4ab42fd1abb60ca3d409
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-07-11 19:06:39 +0300

    ManyToOne, OneToMany decorators

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../users/user.entity';
 
 @Entity()
 export class Report {
@@ -25,4 +26,7 @@ export class Report {
 
   @Column()
   mileage: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,7 +5,9 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -18,18 +20,21 @@ export class User {
   @Column()
   password: string;
 
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
+
   @AfterInsert()
   logInsert() {
-    console.log('Inserted User with id: ' + this.id);
+    console.log('Inserted User with id', this.id);
   }
 
   @AfterUpdate()
   logUpdate() {
-    console.log('Updated User with id: ' + this.id);
+    console.log('Updated User with id', this.id);
   }
 
   @AfterRemove()
   logRemove() {
-    console.log('Removed User with id: ' + this.id);
+    console.log('Removed User with id', this.id);
   }
 }
